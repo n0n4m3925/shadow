8d0671f08559554fc21a9e200b9738bfd119627b
n0n4m3925 <ori0n_pax@tutamail.com>
2025-09-29 14:43:39 +0530

    initial-commit :)

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - The Big Picture v5.0.6 (https://startbootstrap.com/template/the-big-picture)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://drive.proton.me/urls/AK8898GGZW#Xak66lIHHfFg)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
